32e1335a8518be3088101c567fb48038ccc3fc3a
Jessica Allen <jallen@engineyard.com>
2011-12-14 10:59:50 -0800

    A new ice king adventure for the github layout update.

--- github.com.js
+++ github.com.js
@@ -1,5 +1,6 @@
-$('#header .logo img')
-  .css('width', '425px')
-  .css('height', '91px')
-  .css('margin-top', '-15px')
-  .attr('src', 'http://pixelpopdev.com/demos/dotjs/adventuretime.png');
+$('.blacktocat').attr('src', '//adventure-time.herokuapp.com/assets/iceking.png').css({
+  'position': 'absolute',
+  'left': '-54px',
+  'top': '-10px',
+  'width': '56px',
+  'height': '46px'});
